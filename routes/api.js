1b92cfe3d9cba1a9d5288e4decbf3ab4eea3fa2b
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-03-08 20:28:14 +0100

    feat: return full object on POST, allow for filter queries and return filtered array
    Theres not more to explain. Only thing I am worried about is that the FCC test suite
    does not recognize both of these features. Actually both are working. Lets see what happens when I get to the testing part.
    Is it possible that no MongoDB is the problem? I dont really think so, but if it doesnt resolve until then,
    I will have to really use MongoDB. But for the moment, just go on.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 "use strict";
 const { v4: uuidv4 } = require("uuid");
+const { ObjectId } = require("mongodb");
 
 module.exports = function (app) {
 	let container = [
@@ -10,7 +11,7 @@ module.exports = function (app) {
 					assigned_to: "Kamran",
 					status_text: "TBD",
 					open: true,
-					_id: "5871dda29faedc3491ff93bb",
+					_id: new ObjectId(),
 					issue_title: "Test",
 					issue_text: "The initial test issue.",
 					created_by: "Admin",
@@ -26,9 +27,15 @@ module.exports = function (app) {
 		.get(function (req, res) {
 			let project = req.params.project;
 
+			// Lets see if there are filters
+			let filterObject;
+			if (Object.keys(req.query).length > 0) {
+				filterObject = req.query;
+			}
+
 			// see if project exists, if not, create new storage
 			// TODO: SEE IF YOU CAN MOVE THIS RIGHT AFTER app.route. This should work for all routes then i guess.
-			let projectData = findOrCreateData(project);
+			let projectData = findOrCreateData(project, filterObject);
 
 			res.json(projectData);
 		})
@@ -58,7 +65,8 @@ module.exports = function (app) {
 				assigned_to: req.body.assigned_to,
 				status_text: req.body.status_text,
 				open: true,
-				_id: uuidv4().replace(/-/g, "").substring(0, 24),
+				// _id: uuidv4().replace(/-/g, "").substring(0, 24),
+				_id: new ObjectId(),
 				issue_title: req.body.issue_title,
 				issue_text: req.body.issue_text,
 				created_by: req.body.created_by,
@@ -77,7 +85,7 @@ module.exports = function (app) {
 			let project = req.params.project;
 		});
 
-	function findOrCreateData(project) {
+	function findOrCreateData(project, filterObject) {
 		let projectData;
 		// see if this project already exists
 		let found = false;
@@ -88,6 +96,17 @@ module.exports = function (app) {
 				found = true;
 			}
 		}
+
+		//if a project already exists, and filter(s) are sent, apply them
+		if (found === true && filterObject) {
+			const filteredProjectData = projectData.issues.filter((item) => {
+				return Object.keys(filterObject).every(
+					(key) => item[key] === filterObject[key]
+				);
+			});
+			return filteredProjectData;
+		}
+
 		// if its a new project, create the data structure
 		if (!found) {
 			projectData = {
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,7 +7,7 @@ chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
 	//#1
-	test("#test to test some tests", () => {
+	test("#return error without required fields", () => {
 		chai.request(server)
 			.post("/api/issues/testProject")
 			.send({
@@ -21,6 +21,34 @@ suite("Functional Tests", function () {
 					"required field(s) missing",
 					"error did not occur"
 				);
+			});
+	});
+	//#2
+	test("#return correct object with all fields", () => {
+		chai.request(server)
+			.post("/api/issues/testProject")
+			.send({
+				issue_title: "Chai/Mocha Test",
+				issue_text:
+					"Functional unit test to see if the correct object is being returned.",
+				created_by: "Mocha/Chai",
+				assigned_to: "Kamran",
+				status_text: "done",
+			}) // Provide the desired query parameters
+			.end(function (err, res) {
+				assert.equal(err, null); // No error should occur
+				assert.equal(res.status, 200);
+				assert.property(res.body, "assigned_to", "assigned_to missing");
+				assert.property(res.body, "status_text", "status_text missing");
+				assert.property(res.body, "open", "open missing");
+				assert.property(res.body, "_id", "_id missing");
+				assert.property(res.body, "issue_title", "issue_title missing");
+				assert.property(res.body, "issue_text", "issue_text missing");
+				assert.property(res.body, "created_by", "created_by missing");
+				assert.property(res.body, "created_on", "created_on missing");
+				assert.property(res.body, "updated_on", "updated_on missing");
+
+				assert.isBoolean(res.body.open);
 			});
 	});
 });
